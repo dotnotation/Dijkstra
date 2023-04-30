342a334495de8560c41edf5078cc777724be4b58
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-04-30 15:26:44 -0400

    addVertex and addEdge functions added

--- weighted_graph.js
+++ weighted_graph.js
@@ -2,4 +2,18 @@ class WeightedGraph {
     constructor(){
         this.adjacencyList = {}
     }
+    
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+    }
+
+    addEdge(v1, v2, weight){
+        this.adjacencyList[v1].push({node: v2, weight})
+        this.adjacencyList[v2].push({node: v1, weight})
+    }
+}
+
+{
+    "A": [{node: "B", weight: 10}]
+    // essentially the weight is the distance between to vertices 
 }
