59e6fa86f8641d9c6295d769ea191c34c36e2609
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-05-03 13:11:45 -0400

    added notes on how the algo works

---
+++ dijkstra.js
@@ -0,0 +1,6 @@
+// every time you visit a new node
+    // pick the node with the smallest known distance to visit first
+    // look at the neighbors
+    // calculate the distance between neighbors and smallest
+        // sum the total edges
+    // if the new total distance is less than the previous total, store that distance
